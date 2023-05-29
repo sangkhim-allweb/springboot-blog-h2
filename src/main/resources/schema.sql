ccd9427e279f0e386a76e5f32f0363915b6086ea
sangkhim <sangkhim.allweb@gmail.com>
2023-05-29 08:49:58 +0700

    add foreign key

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,30 +1,34 @@
 DROP TABLE IF EXISTS post_tags;
-CREATE TABLE post_tags
-(
-  post_id INT,
-  tag_id  INT,
-  PRIMARY KEY (post_id, tag_id)
-);
-
 DROP TABLE IF EXISTS posts;
-CREATE TABLE posts
-(
-  id        INT AUTO_INCREMENT PRIMARY KEY,
-  title     VARCHAR(250) NOT NULL,
-  body      VARCHAR(250) NOT NULL,
-  author_id INT
-);
-
 DROP TABLE IF EXISTS authors;
+DROP TABLE IF EXISTS tags;
+
 CREATE TABLE authors
 (
   id   INT AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(250) NOT NULL
 );
 
-DROP TABLE IF EXISTS tags;
 CREATE TABLE tags
 (
   id   INT AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(250) NOT NULL
+);
+
+CREATE TABLE posts
+(
+    id        INT AUTO_INCREMENT PRIMARY KEY,
+    title     VARCHAR(250) NOT NULL,
+    body      VARCHAR(250) NOT NULL,
+    author_id INT,
+    FOREIGN KEY (author_id) REFERENCES authors(id)
+);
+
+CREATE TABLE post_tags
+(
+    post_id INT,
+    tag_id  INT,
+    PRIMARY KEY (post_id, tag_id),
+    FOREIGN KEY (post_id) REFERENCES posts(id),
+    FOREIGN KEY (tag_id) REFERENCES tags(id)
 );
