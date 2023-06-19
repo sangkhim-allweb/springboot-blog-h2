175ee27541495153f8396dfc557860bf71836297
sangkhim <sangkhim.allweb@gmail.com>
2023-06-19 13:30:40 +0700

    jpa pagination

--- src/main/java/com/allweb/springbootblogh2/repository/PostRepository.java
+++ src/main/java/com/allweb/springbootblogh2/repository/PostRepository.java
@@ -1,6 +1,8 @@
 package com.allweb.springbootblogh2.repository;
 
 import com.allweb.springbootblogh2.model.entity.Post;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -17,6 +19,12 @@ public interface PostRepository extends JpaRepository<Post, Long> {
     @Modifying
     @Query(value = "SELECT * FROM posts WHERE title LIKE %:title%", nativeQuery = true)
     List<Post> selectPosts(@Param("title") String title);
+
+    @Query(
+            value = "SELECT * FROM Users ORDER BY id",
+            countQuery = "SELECT count(*) FROM Users",
+            nativeQuery = true)
+    Page<Post> findAllPostsWithPagination(Pageable pageable);
 
     @Modifying
     @Query("DELETE FROM Post p WHERE p.title = :title OR p.body = :body")
