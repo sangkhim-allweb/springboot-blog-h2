6c61cd184e860dbc6904b48b032ac2ee83d1cfc8
sangkhim <sangkhim.allweb@gmail.com>
2023-06-19 13:19:29 +0700

    jpa query

--- src/main/java/com/allweb/springbootblogh2/repository/PostRepository.java
+++ src/main/java/com/allweb/springbootblogh2/repository/PostRepository.java
@@ -2,6 +2,9 @@ package com.allweb.springbootblogh2.repository;
 
 import com.allweb.springbootblogh2.model.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,5 +13,13 @@ import java.util.List;
 public interface PostRepository extends JpaRepository<Post, Long> {
 
     List<Post> findByTitleContaining(String title);
+
+    @Modifying
+    @Query(value = "SELECT * FROM posts WHERE title LIKE %:title%", nativeQuery = true)
+    List<Post> selectPosts(@Param("title") String title);
+
+    @Modifying
+    @Query("DELETE FROM Post p WHERE p.title = :title OR p.body = :body")
+    int deletePosts(@Param("title") String title, @Param("body") String body);
 
 }
